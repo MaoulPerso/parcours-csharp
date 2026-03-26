42b7e51d5f188dcac4eb79b36682790dc49d396b
Zelephir1 <mael.caetano@ynov.com>
2026-03-26 10:40:27 +0100

    AreEquals

---
+++ AreEquals.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace CSharpDiscovery.Quest02
+{
+    public class AreEquals_Exercice
+    {
+        public static bool AreEquals(int[] tab1, int[] tab2)
+        {
+            if (tab1 == null && tab2 == null) return true;
+            if (tab1 == null || tab2 == null) return false;
+            if (tab1.Length != tab2.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < tab1.Length; i++)
+            {
+                if (tab1[i] != tab2[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
