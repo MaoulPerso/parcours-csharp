7113557e40d9c38a5db77861a2301e3a4c7c9123
Zelephir1 <mael.caetano@ynov.com>
2026-03-26 11:46:54 +0100

    PointOfInterest

---
+++ PointOfInterest.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CSharpDiscovery.Quest03
+{
+    public class PointOfInterest
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Name { get; set; }
+        public static string GoogleMapsUrlTemplate { get; set; } = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
+    }
+}
