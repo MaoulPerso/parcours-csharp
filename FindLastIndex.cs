acafee637d692822919c8cca4ea08ca501ab54b8
Zelephir1 <mael.caetano@ynov.com>
2026-03-26 10:49:43 +0100

    FindLastIndex

---
+++ FindLastIndex.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace CSharpDiscovery.Quest02
+{
+    public class FindLastIndex_Exercice
+    {
+        public static int? FindLastIndex(int[] tab, int a)
+        {
+            if (tab == null) return null;
+            for (int i = tab.Length - 1; i >= 0; i--)
+            {
+                if (tab[i] == a)
+                {
+                    return i;
+                }
+            }
+            return null;
+        }
+    }
+}
