a18cb62beb2284fcd80ecc4097b50f3ac20a6111
Mael CAETANO <mael.caetano@ynov.com>
2026-04-09 09:37:28 +0200

    PointOfInterest

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -7,6 +7,7 @@ namespace CSharpDiscovery.Quest03
         public double Latitude { get; set; }
         public double Longitude { get; set; }
         public string Name { get; set; }
+
         public static string GoogleMapsUrlTemplate { get; set; } = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
     }
 }
